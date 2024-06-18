8945bf9cf93d1c42e27a42acd68c1e774f3e6058
Ankit-KDs <ak30kr02@gmail.com>
2024-06-18 11:27:36 +0000

    type conversion

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -3,3 +3,8 @@ console.log("ankit kumar")
 console.log('5'+5);
 let num = '5'+5;
 console.log(typeof num);
+console.log(parseInt('F12345'));
+console.log(parseInt(123.34));
+console.log(Number('Value'));
+console.log(Number(true));
+console.log(String(undefined));
