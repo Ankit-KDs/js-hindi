5e7df28972a93eff5abffb9c70010a4fbd32e9c2
Ankit-KDs <ak30kr02@gmail.com>
2024-06-18 11:35:22 +0000

    variables

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -8,3 +8,20 @@ console.log(parseInt(123.34));
 console.log(Number('Value'));
 console.log(Number(true));
 console.log(String(undefined));
+
+// Variables
+
+let age = 10;
+age = 20;
+console.log(age);
+let name;
+name = "Roman";
+console.log(name);
+const pi = 3.14;
+// pi = 22;
+
+var Class = 5;
+var Class = 8;
+Class = 9;
+
+console.log(Class);
